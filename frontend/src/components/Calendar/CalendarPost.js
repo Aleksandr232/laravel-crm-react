2ed133980a62a8d5da2e17ec850f18b68185817f
Aleksandr232 <aleksrowing232@gmail.com>
2024-03-28 13:48:56 +0300

    calendar

--- frontend/src/components/Calendar/CalendarPost.js
+++ frontend/src/components/Calendar/CalendarPost.js
@@ -1,41 +1,63 @@
-import React, { useState, useContext } from "react";
-import FullCalendar from '@fullcalendar/react' 
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from "@fullcalendar/interaction"
-import timeGridPlugin from '@fullcalendar/timegrid';
+import { useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction"; 
+import ruLocale from '@fullcalendar/core/locales/ru'; // Добавлен локаль для русского языка
+import { v4 as uuid } from "uuid";
+
+const EventItem = ({ info }) => {
+    const { event } = info;
+    return (
+      <div>
+        <p>{event.title}</p>
+      </div>
+    );
+  };
 
 const CalendarPost = () => {
-    const [events, setEvents] = useState([
-        { 
-            title: 'Уборка снега', 
-            start: '2024-03-28T20:00:00', 
-            end: '2024-03-29T10:00:00'
-          },
-          {  
-            title: 'Уборка снега2', 
-            start: '2024-03-28T10:00:00', 
-            end: '2024-03-29T20:00:00'
-          } //Переставляем месяц и день в формате даты
-    ]);
+  const [events, setEvents] = useState([]);
 
-    const calendarRef = useContext(FullCalendar);
+  const handleSelect = (info) => {
+    const { start, end } = info;
+    const eventNamePrompt = prompt("Enter, event name");
+    if (eventNamePrompt) {
+      setEvents([
+        ...events,
+        {
+          start,
+          end,
+          title: eventNamePrompt,
+          id: uuid()
+        }
+      ]);
+    }
+  };
 
-    return (
-        <div className="w-full max-w-screen-xl mx-auto p-4">
-            <FullCalendar
-            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
-            initialView='dayGridMonth' 
-            locale="ru"
-            events={events}
-            headerToolbar={{
-                start: 'title prev,next today',
-                center: 'dayGridMonth,timeGridWeek,timeGridDay',
-                end: 'listWeek'
-            }}
-            className="mt-4"
-        />
-        </div>
-    )
-}
+  return (
+    <div className="w-full max-w-screen-xl mx-auto p-4">
+      <FullCalendar
+        editable
+        selectable
+        events={events}
+        select={handleSelect}
+        headerToolbar={{
+          start: "today prev next",
+          end: "dayGridMonth dayGridWeek dayGridDay"
+        }}
+        eventContent={(info) => <EventItem info={info} />}
+        plugins={[dayGridPlugin, interactionPlugin]}
+        views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
+        locales={[ruLocale]}
+        slotMinTime="00:00"
+        slotMaxTime="24:00"
+      />
+    </div>
+  );
+};
 
 export default CalendarPost;
+
+
+
+
+
