c6e94d1b646f7252e5abaae52c551b526cedeb8f
Aleksandr232 <aleksrowing232@gmail.com>
2024-04-04 16:00:46 +0300

    modal_clients

--- frontend/src/components/ClientsWorks/ClientsWorksTable.js
+++ frontend/src/components/ClientsWorks/ClientsWorksTable.js
@@ -3,6 +3,15 @@ import axios from "axios";
 
 const ClientsWorksTable=()=>{
     const [data, setData] = useState([]);
+    const [selectedClient, setSelectedClient] = useState(null);
+
+    const openModal = (clients) => {
+        setSelectedClient(clients);
+      };
+    
+      const closeModal = () => {
+        setSelectedClient(null);
+      };
 
     useEffect(() => {
         const token = localStorage.getItem("token");
@@ -33,7 +42,7 @@ const ClientsWorksTable=()=>{
         </thead>
         <tbody className="divide-y divide-gray-200">
         {data.map((clients, id) => (
-        <tr key={id}>
+        <tr key={id} onClick={() => openModal(clients)}>
         <td className="px-3 py-4 text-center whitespace-nowrap">{clients.name}</td>
         <td className="px-3 py-4 text-center whitespace-nowrap">{clients.phone}</td>
         <td className="px-3 py-4 text-center whitespace-nowrap">{clients.organization}</td>
@@ -43,6 +52,23 @@ const ClientsWorksTable=()=>{
     ))}
         </tbody>
     </table>
+    {selectedClient && (
+        <div className="fixed z-10 inset-0 flex items-center justify-center">
+          <div className="fixed inset-0 transition-opacity">
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+          <div className="bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 align-middle p-6 w-3/4">
+            <span className="close absolute top-0 right-0 m-4 cursor-pointer text-gray-500" onClick={closeModal}>&times;</span>
+            <h3 className="text-lg font-semibold mb-2">Данные клиента</h3>
+            <p>Имя: {selectedClient.name}</p>
+            <p>Телефон: {selectedClient.phone}</p>
+            <p>Организация: {selectedClient.organization}</p>
+            <p>Тип работы: {selectedClient.type_work}</p>
+            <p>Срок работы: {selectedClient.duration}</p>
+            <p>Дополнительная информация: {selectedClient.additional_info}</p>
+          </div>
+        </div>
+      )}
 </div>
     )
 }
